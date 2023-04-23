4715f8cb8295184c5e6c7da73d9129a4c0281486
Asad Yasin <51986208+asadyasin@users.noreply.github.com>
2023-04-23 09:03:16 +0500

    limit extended

--- index.js
+++ index.js
@@ -18,8 +18,8 @@ app.use(express.static('public'));
 app.use(express.json({}));
 app.use(compression());
 { }
-app.use(bodyParser.json({ limit: '50mb', extended: true}));
-app.use(bodyParser.urlencoded({ limit: '50mb', extended: true}));
+app.use(bodyParser.json({ limit: '100mb', extended: true}));
+app.use(bodyParser.urlencoded({ limit: '100mb', extended: true}));
 app.use(cors());
 
 app.use('/degrees', degreeRoutes);
